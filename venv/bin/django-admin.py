9fa9be432219d9a127b9472c23ae1971ebc95003
innocent lou <bisonlou@gmail.com>
2019-04-16 11:28:05 +0300

    Removed virtual enviroment

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/innocentlou/Documents/Python/Django/ireporter/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
