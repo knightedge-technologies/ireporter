d18007a404c6796d0a3418cd060d71bca18c0804
nadralia <nadralia@gmail.com>
2019-04-18 09:10:00 +0300

    Ch(Setup Swagger): Api Documentation
    Added drf_yasg dependency for swagger integration
    changed the setting.py file
    Added the urls for documentation called (docs)
    
    [Finishes #165416463]

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from django.contrib.auth.password_validation import ValidationError
 from .serializers import UserSerializer, LoginSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 
 class SignUp(APIView):
@@ -14,7 +15,12 @@ class SignUp(APIView):
     """
 
     serializer_class = UserSerializer
-
+    @swagger_auto_schema(
+        operation_description="Create a user account.",
+        operation_id="Sign up a user",
+        request_body=UserSerializer,
+        responses={201: UserSerializer(many=False), 400: "BAD REQUEST"},
+    )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -53,6 +59,12 @@ class Login(APIView):
     """
 
     serializer_class = LoginSerializer
+    @swagger_auto_schema(
+        operation_description="Login a User",
+        operation_id="Login a user",
+        request_body=UserSerializer,
+        responses={200: UserSerializer(many=False), 401: "Invalid Login"},
+    )
     def post(self, request, *args, **kwargs):
 
         serializer = LoginSerializer(data=request.data)
